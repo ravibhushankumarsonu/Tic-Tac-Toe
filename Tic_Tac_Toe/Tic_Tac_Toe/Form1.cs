47aaa8c7dc313d61fcf0bd9cb3c56e0562a4f07c
Ravibhushan Kumar <ravibhushankumarsonu@gmail.com>
2016-12-30 00:39:54 +0530

    Add files via upload

---
+++ Tic_Tac_Toe/Tic_Tac_Toe/Form1.cs
@@ -0,0 +1,148 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Tic_Tac_Toe
+{
+    public partial class Form1 : Form
+    {
+        bool turn = true; //If true x turn 
+                          // If false 0 turn
+        int turn_count = 0;
+        //varile for both player
+        static string p1, p2;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button_Click(object sender, EventArgs e)
+        {
+            Button bt = (Button)sender;
+            if(turn)
+            {
+                bt.Text = "X";
+            }
+            else
+            {
+                bt.Text = "0";
+            }
+            turn_count++;
+            turn = !turn;
+            bt.Enabled = false;
+            Check_Winner();
+            if(turn_count==9)
+            {
+                MessageBox.Show("Game is Draw", "Draw");
+                draw_count.Text=(Int32.Parse(draw_count.Text)+1).ToString();
+            }
+        }
+        private void Check_Winner()
+        {
+            bool winner = false;
+            if((A1.Text==A2.Text)&&(A2.Text==A3.Text)&&(!A1.Enabled))
+                {
+                winner = true;
+            }
+            if ((B1.Text == B2.Text) && (B2.Text == B3.Text) && (!B1.Enabled))
+            {
+                winner = true;
+            }
+            if ((C1.Text == C2.Text) && (C2.Text == C3.Text) && (!C1.Enabled))
+            {
+                winner = true;
+            }
+            //End of the row check
+            //Start of the column check
+            if ((A1.Text == B1.Text) && (B1.Text == C1.Text) && (!A1.Enabled))
+            {
+                winner = true;
+            }
+            if ((A2.Text == B2.Text) && (B2.Text == C2.Text) && (!A2.Enabled))
+            {
+                winner = true;
+            }
+            if ((A3.Text == B3.Text) && (B3.Text == C3.Text) && (!A3.Enabled))
+            {
+                winner = true;
+            }
+            //End of the Column  check
+            //Start of the diagonal check
+            if ((A1.Text == B2.Text) && (B2.Text == C3.Text) && (!A1.Enabled))
+            {
+                winner = true;
+            }
+            if ((A3.Text == B2.Text) && (B2.Text == C1.Text) && (!A3.Enabled))
+            {
+                winner = true;
+            }
+            //End of the diagonal check
+            if (winner)
+            {
+                if(turn)
+                {
+                    o_win_count.Text = (Int32.Parse(o_win_count.Text) + 1).ToString();
+                    MessageBox.Show("0 is Winner", "Winner");
+                }
+                else
+                {
+                    x_win_count.Text = (Int32.Parse(x_win_count.Text) + 1).ToString();
+                    MessageBox.Show("X is Winner", "Winner");
+                }
+                
+                    foreach (Control item in Controls)
+                    {
+                    try
+                    {
+                        Button bt = (Button)item;
+                        bt.Enabled = false;
+                        // bt.Text = "";
+                    }
+                    catch (Exception)
+                    { }
+                }
+                
+            }
+        }
+        private void helpToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Tic Tac Toe is under construction", "Help Documentation");
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Form2 f2 = new Form2();
+            f2.ShowDialog();
+        }
+
+        private void newGameToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            turn = true;
+            turn_count = 0;
+            
+                foreach (Control item in Controls)
+                {
+                try
+                {
+                    Button bt = (Button)item;
+                    bt.Enabled = true;
+                    bt.Text = "";
+                }
+                catch (Exception)
+                { }
+            }
+           
+        }
+    }
+}
---
+++ Tic_Tac_Toe/Tic_Tac_Toe/Form2.Designer.cs
@@ -0,0 +1,111 @@
+﻿namespace Tic_Tac_Toe
+{
+    partial class Form2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.player2 = new System.Windows.Forms.TextBox();
+            this.player1 = new System.Windows.Forms.TextBox();
+            this.play = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(5, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(140, 18);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Player One Name";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(5, 45);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(141, 18);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Player Two Name";
+            // 
+            // player2
+            // 
+            this.player2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.player2.Location = new System.Drawing.Point(152, 37);
+            this.player2.Name = "player2";
+            this.player2.Size = new System.Drawing.Size(232, 26);
+            this.player2.TabIndex = 1;
+            // 
+            // player1
+            // 
+            this.player1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.player1.Location = new System.Drawing.Point(151, 5);
+            this.player1.Name = "player1";
+            this.player1.Size = new System.Drawing.Size(232, 26);
+            this.player1.TabIndex = 1;
+            // 
+            // play
+            // 
+            this.play.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.play.Location = new System.Drawing.Point(303, 70);
+            this.play.Name = "play";
+            this.play.Size = new System.Drawing.Size(75, 29);
+            this.play.TabIndex = 2;
+            this.play.Text = "Play";
+            this.play.UseVisualStyleBackColor = true;
+            this.play.Click += new System.EventHandler(this.play_Click);
+            // 
+            // Form2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(397, 110);
+            this.Controls.Add(this.play);
+            this.Controls.Add(this.player1);
+            this.Controls.Add(this.player2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "Form2";
+            this.Text = "Tic Tac Toe";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox player2;
+        private System.Windows.Forms.TextBox player1;
+        private System.Windows.Forms.Button play;
+    }
+}
